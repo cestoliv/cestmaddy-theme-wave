240cb045c1f7262541e73f0a8334192c904b0fe1
Olivier Cartier <me@cestoliv.com>
2021-02-26 09:21:13 +0100

    Update linux-fast-install.sh

--- linux-fast-install.sh
+++ linux-fast-install.sh
@@ -1,5 +1,5 @@
 # DOWNLOAD REPOSITORY
-rm -Rf res/content/front/themes/wave
+rm -rdf res/content/front/themes/wave
 git clone https://github.com/cestoliv/cestmaddy-theme-wave.git res/content/front/themes/wave
 
 # CHANGE THEME IN CONFIG
